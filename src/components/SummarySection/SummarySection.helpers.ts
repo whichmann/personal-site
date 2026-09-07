b33015d5bb30fd83c606f1c4bc47d3ae5cf6be7d
Karol <karol.wichmann@outlook.com>
2026-09-07 22:16:39 +0200

    update summary texts

--- src/components/SummarySection/SummarySection.helpers.ts
+++ src/components/SummarySection/SummarySection.helpers.ts
@@ -8,28 +8,28 @@ export type SummaryItem = {
 export const summaryItems: SummaryItem[] = [
   {
     id: 'TIS',
-    label: 'I am a front-end developer at Treasury Intelligence Solutions, creating next-generation Cash Forecasting platform',
+    label: 'At TIS, I develop next-generation Cash Forecasting platform',
     imageSrc: '/tis-logo.png',  
     description:
-      'Worked on a large-scale Cash Forecasting platform, building data-intensive web applications. Built real-time data handling features, ensuring seamless synchronization between user input and multiple data sources',
+      'Working on a large-scale Cash Forecasting platform, building data-intensive web applications. Built real-time data handling features, ensuring seamless synchronization between user input and multiple data sources',
   },
   {
-    id: 'PwC',
-    label: 'I was a Senior Associate at PwC Poland, working on complex platform, helping fight financial crime and money laundering',
+    id: 'PwC-fe',
+    label: 'I was a Senior Associate at PwC, helping fight financial crimes',
     imageSrc: '/pwc-logo.svg',
     description:
       'An overview of the technologies, practices, and ways of working I use to turn ideas into useful products. This section will grow with the tools and disciplines that shape my work.',
   },
   {
-    id: 'PwC',
-    label: 'I started my career in IT as a Data Analyst, where I learned how to create meaningful reports and dashboards, and how to work with large datasets',
+    id: 'PwC-da',
+    label: 'My IT career in Data Analysis, where I learned how to work with large datasets',
     imageSrc: '/pwc-logo-old.png',
     description:
       'Built reports and dashboards using Tableau, Power BI, Excel, SQL, and MS Access. Obtained a strong foundation in data analysis, visualization, and reporting, which has been invaluable in my career as a front-end developer.',
   },
   {
     id: 'Side Projects',
-    label: 'I do side projects in my free time, exploring new technologies and building things that interest me',
+    label: 'In my free time, I explore new technologies and build things that interest me',
     imageSrc: '/side-projects.gif',
     description:
       'The best ways to get in touch about a project, collaboration, or conversation. Contact details and relevant links will be added here.',
